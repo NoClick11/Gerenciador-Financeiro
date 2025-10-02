4ea36c8d80ec1459210bce97f491f110b5f5f609
NoClick11 <bahamonde.boullosa@hotmail.com>
2025-10-02 17:04:27 -0300

    feat: Delete function add

--- frontend/src/pages/TransactionListPage.jsx
+++ frontend/src/pages/TransactionListPage.jsx
@@ -1,16 +1,12 @@
-
 import { useState, useEffect } from 'react';
 import TransactionForm from '../components/TransactionForm';
 
 function TransactionListPage() {
-
   const [transactions, setTransactions] = useState([]);
-
 
   useEffect(() => {
     const fetchTransactions = async () => {
       try {
-
         const response = await fetch('http://localhost:8080/api/transactions');
         const data = await response.json();
         setTransactions(data);
@@ -22,25 +18,44 @@ function TransactionListPage() {
     fetchTransactions();
   }, []);
 
+  const handleDelete = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:8080/api/transactions/${id}`, {
+        method: 'DELETE',
+      });
+
+      if (response.ok) {
+
+        setTransactions(currentTransactions =>
+          currentTransactions.filter(transaction => transaction.id !== id)
+        );
+        console.log(`Transação com id ${id} deletada com sucesso!`);
+      } else {
+        throw new Error('Falha ao deletar a transação');
+      }
+    } catch (error) {
+      console.error("Houve um erro ao deletar a transação:", error);
+    }
+  };
+
   return (
     <div>
       <h1>Gerenciador Financeiro</h1>
-
-      {}
       <TransactionForm />
-
-      <hr /> {}
-
+      <hr />
       <h2>Lista de Transações</h2>
       <ul>
-        {}
         {transactions.map(transaction => (
           <li key={transaction.id}>
-            {}
-            <span>{transaction.date}</span> | 
-            <span> {transaction.description} </span> | 
-            <span>{transaction.type}</span> | 
+            <span>{transaction.date}</span> |
+            <span> {transaction.description} </span> |
+            <span>{transaction.type}</span> |
             <strong> R$ {transaction.amount}</strong>
+            {}
+            {}
+            <button onClick={() => handleDelete(transaction.id)} style={{ marginLeft: '10px' }}>
+              X
+            </button>
           </li>
         ))}
       </ul>
