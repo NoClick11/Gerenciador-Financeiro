95bd33207dd805d4b64d744b0f9655f03f839a11
NoClick11 <bahamonde.boullosa@hotmail.com>
2025-10-01 22:57:50 -0300

    feaet: React add

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import TransactionListPage from './pages/TransactionListPage'
+
+function App() {
+  return (
+    <div>
+      <TransactionListPage />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/TransactionListPage.jsx
@@ -0,0 +1,10 @@
+function TransactionListPage() {
+  return (
+    <div>
+      <h2>Lista de Transações</h2>
+      {/* Futuramente, aqui vamos exibir os dados da nossa API */}
+    </div>
+  );
+}
+
+export default TransactionListPage;
