1c328748bdaae437740fe70eacda37e35d953c0c
NoClick11 <bahamonde.boullosa@hotmail.com>
2025-10-01 23:14:18 -0300

    feat: TransactionListPage add

--- frontend/src/pages/TransactionListPage.jsx
+++ frontend/src/pages/TransactionListPage.jsx
@@ -1,8 +1,35 @@
+import { useState, useEffect } from 'react';
+
 function TransactionListPage() {
+
+  const [transactions, setTransactions] = useState([]);
+
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/api/transactions');
+        const data = await response.json();
+        setTransactions(data);
+      } catch (error) {
+        console.error("Erro ao buscar transações:", error);
+      }
+    };
+
+    fetchTransactions();
+  }, []);
+
   return (
     <div>
       <h2>Lista de Transações</h2>
-      {/* Futuramente, aqui vamos exibir os dados da nossa API */}
+      <ul>
+        {}
+        {transactions.map(transaction => (
+          <li key={transaction.id}>
+            {transaction.date} | {transaction.description} | {transaction.type} | R$ {transaction.amount}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
